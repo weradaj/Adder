cef505b5b5d68fc430df19e7042fdfcab1fa4475
weradaj <43135733+weradaj@users.noreply.github.com>
2018-09-10 19:16:59 +0700

    Add files via upload

---
+++ Adder1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Adder1 {
+    public static void main (String[]args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = x+y;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
+        
+     
+    
